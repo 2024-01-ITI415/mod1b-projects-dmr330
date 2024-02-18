b2d7392c53a5adcb9e7bcdb92598be55291d4e8b
dmr330 <dmr330@scarletmail.rutgers.edu>
2024-02-18 14:22:17 -0500

    Gravity settings
    Made it so I could adjust the gravity on my sphere

--- Assets/03-Prototype1/Scripts/Bird.cs
+++ Assets/03-Prototype1/Scripts/Bird.cs
@@ -6,13 +6,26 @@ public class NewBehaviourScript : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public Rigidbody rb;
+    public float gravityScale = 1.0f;
+    public float globalGravity = -10.0f;
     public float jumpAmount = 10;
 
-    void Update()
+    void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
         }
+    }
+    void FixedUpdate()
+    {
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        rb.AddForce(gravity, ForceMode.Acceleration);
     }
 }
